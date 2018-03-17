fb7346a6a94cece637590b54b2e0be88bbef04e1
thanhnv <nvthanh15490@gmail.com>
2018-03-17 20:40:10 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import bottle
+import os
+from bottle import get, run, route, template, static_file, request, response
+from os import environ
+from dao import get_dao
+
+application = bottle.app()
+dao = get_dao('production')
+
+@route('/assets/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='./public')
+
+@application.route('/', method=['GET'])
+def home():
+    return template('./public/ThanhDuyen.html')
+
+if __name__ == "__main__":
+ application.run(host='0.0.0.0', port=int(environ.get('PORT', 8000)))
